98959b51fae8106e9bdb5ee7286417e8153d1dc7
Sidney Kochman <sidney@kochman.org>
2016-02-05 14:50:47 -0500

    Add connection manager to manage WebSockets

--- main.go
+++ main.go
@@ -1,22 +1,61 @@
 package main
 
 import (
-	"io"
-	"net/http"
 	"golang.org/x/net/websocket"
+	"net/http"
 )
 
+var cm *connectionManager
+
 func EchoServer(ws *websocket.Conn) {
-	io.Copy(ws, ws)
+	cm.register <- ws
+	for {
+		buf := make([]byte, 256)
+		ws.Read(buf)
+		cm.broadcast <- buf
+	}
 }
 
 func StaticServer(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "static")
 }
 
+type connectionManager struct {
+	connections map[*websocket.Conn]struct{} // empty struct uses zero bytes
+	register    chan *websocket.Conn
+	broadcast   chan []byte
+}
+
+func newConnectionManager() *connectionManager {
+	return &connectionManager{
+		connections: make(map[*websocket.Conn]struct{}),
+		register:    make(chan *websocket.Conn),
+		broadcast:   make(chan []byte),
+	}
+}
+
+func (cm *connectionManager) run() {
+	for {
+		select {
+		case ws := <-cm.register:
+			cm.connections[ws] = struct{}{}
+		case m := <-cm.broadcast:
+			for ws := range cm.connections {
+				if _, err := ws.Write(m); err != nil {
+					delete(cm.connections, ws)
+				}
+			}
+		}
+	}
+}
+
 func main() {
 	http.HandleFunc("/", StaticServer)
 	http.Handle("/echo", websocket.Handler(EchoServer))
+
+	cm = newConnectionManager()
+	go cm.run()
+
 	err := http.ListenAndServe(":12345", nil)
 	if err != nil {
 		panic("ListenAndServe: " + err.Error())
